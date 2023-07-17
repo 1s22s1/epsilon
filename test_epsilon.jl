d905098ad27f2ad720eb59751a5c507a0d7af0ff
1s22s1 <1s22s1@example.com>
2023-07-17 09:34:47 +0900

    Seiri

--- test_epsilon.jl
+++ test_epsilon.jl
@@ -3,27 +3,22 @@ module TestEpsilon
     include("epsilon.jl")
 
     function main()
-        @testset "選択の場合" begin
-            arrows = []
-            push!(arrows, Epsilon.Arrow(0, 1, "ϵ"))
-            push!(arrows, Epsilon.Arrow(0, 2, "ϵ"))
-            push!(arrows, Epsilon.Arrow(1, 3, "0"))
-            push!(arrows, Epsilon.Arrow(2, 4, "1"))
-            push!(arrows, Epsilon.Arrow(3, 5, "ϵ"))
-            push!(arrows, Epsilon.Arrow(4, 5, "ϵ"))
+        arrows = []
+        push!(arrows, Epsilon.Arrow(0, 1, "ϵ"))
+        push!(arrows, Epsilon.Arrow(0, 2, "ϵ"))
+        push!(arrows, Epsilon.Arrow(1, 3, "0"))
+        push!(arrows, Epsilon.Arrow(2, 4, "1"))
+        push!(arrows, Epsilon.Arrow(3, 5, "ϵ"))
+        push!(arrows, Epsilon.Arrow(4, 5, "ϵ"))
 
-            # @test Epsilon.isaccept(arrows, [5], "0") == true
+        @testset "ϵ遷移の判定について" begin
+            @test Epsilon.needEpsilonTransition([0], arrows) == true
+            @test Epsilon.needEpsilonTransition([1, 2], arrows) == false
+            @test Epsilon.needEpsilonTransition([3], arrows) == true
+            @test Epsilon.needEpsilonTransition([4], arrows) == true
         end
 
         @testset "遷移について" begin
-            arrows = []
-            push!(arrows, Epsilon.Arrow(0, 1, "ϵ"))
-            push!(arrows, Epsilon.Arrow(0, 2, "ϵ"))
-            push!(arrows, Epsilon.Arrow(1, 3, "0"))
-            push!(arrows, Epsilon.Arrow(2, 4, "1"))
-            push!(arrows, Epsilon.Arrow(3, 5, "ϵ"))
-            push!(arrows, Epsilon.Arrow(4, 5, "ϵ"))
-
             @test Epsilon.transition([0], arrows, "ϵ") == [1, 2]
             @test Epsilon.transition([1, 2], arrows, "0") == [3]
             @test Epsilon.transition([1, 2], arrows, "1") == [4]
@@ -32,19 +27,8 @@ module TestEpsilon
             @test Epsilon.transition([4], arrows, "ϵ") == [5]
         end
 
-        @testset "ϵ遷移の判定について" begin
-            arrows = []
-            push!(arrows, Epsilon.Arrow(0, 1, "ϵ"))
-            push!(arrows, Epsilon.Arrow(0, 2, "ϵ"))
-            push!(arrows, Epsilon.Arrow(1, 3, "0"))
-            push!(arrows, Epsilon.Arrow(2, 4, "1"))
-            push!(arrows, Epsilon.Arrow(3, 5, "ϵ"))
-            push!(arrows, Epsilon.Arrow(4, 5, "ϵ"))
-
-            @test Epsilon.needEpsilonTransition([0], arrows) == true
-            @test Epsilon.needEpsilonTransition([1, 2], arrows) == false
-            @test Epsilon.needEpsilonTransition([3], arrows) == true
-            @test Epsilon.needEpsilonTransition([4], arrows) == true
+        @testset "選択の場合" begin
+            # @test Epsilon.isaccept(arrows, [5], "0") == true
         end
     end
 end
